17c0a698a08188d7bcf047926c45dbcd43b221ca
Eduardo Ruano Carrón <eduruacar@alu.edu.gva.es>
2023-11-29 13:39:28 +0100

    Primera Versión

---
+++ src/pruebaP05.java
@@ -0,0 +1,19 @@
+public class pruebaP05 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\n¡Hola, soy Eduardo Ruano Carrón!\n") ;
+
+        System.out.println("Voy a escribir una serie de números del 1 al 10: \n") ;
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("Nº = " + i) ;
+
+        }
+
+        System.out.println("\n¡Un saludo, adiós!\n") ;
+
+    }
+
+}
