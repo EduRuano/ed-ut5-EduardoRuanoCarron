dd74dcf85209aa2aedce1eea608326e481d88e41
JOSJOV <josjovper@alu.edu.gva.es>
2023-11-30 09:30:39 +0100

    Serie de numeros aumentada a 15

--- src/pruebaP05.java
+++ src/pruebaP05.java
@@ -4,9 +4,9 @@ public class pruebaP05 {
 
         System.out.println("\n¡Hola, soy Eduardo Ruano Carrón!\n") ;
 
-        System.out.println("Voy a escribir una serie de números del 1 al 10: \n") ;
+        System.out.println("Voy a escribir una serie de números del 1 al 15: \n") ;
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 15; i++) {
 
             System.out.println("Nº = " + i) ;
 
